f39a3bbc362c4e75149048525bb1a0561720fc77
jamie brim <jwb@tu.nr>
2013-07-10 22:18:45 -0400

    ruby is not coffeescript

--- lib/sidekiq-unique-jobs/worker.rb
+++ lib/sidekiq-unique-jobs/worker.rb
@@ -1,7 +1,8 @@
 module Sidekiq
   module Worker
     def unlock(*args)
-      klass, queue = self.class, klass.get_sidekiq_options['queue']
+      klass = self.class
+      queue = klass.get_sidekiq_options['queue']
       payload = SidekiqUniqueJobs::PayloadHelper.get_payload(klass, queue, args)
       Sidekiq.redis {|conn| conn.del(payload) }
     end
